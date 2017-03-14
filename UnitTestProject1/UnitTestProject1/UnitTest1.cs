5650e680b9f5312808368f09346b7ec6f97ce026
eso31 <eso31@users.noreply.github.com>
2017-03-13 18:56:38 -0700

    Add files via upload
    Practica Unit Testing con la funcion hash

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTestingPractice;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Int64 x = Program.Hash("leepadg");
+
+            Assert.AreEqual(x, 680131659347);
+        }
+    }
+}
---
+++ UnitTestingPractice/UnitTestingPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingPractice
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string letters = "leepadg";
+            Console.WriteLine(Hash(letters));
+            Console.Read();
+        }
+
+        public static Int64 Hash(string s)
+        {
+
+            Int64 h = 7;
+            Int64 inc=0;
+            Int64 index=0;
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                string letters = "acdegilmnoprstuw";
+                inc = h * 37;
+                index = letters.IndexOf((char)s[i]);
+                h = inc + index;
+            }
+
+            return h;
+        }
+    }
+}
